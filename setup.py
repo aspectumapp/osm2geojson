e2db0342f81f24dd5df8888d2793fdbb2b558fdf
rapkin <mikola.parfenyuck@gmail.com>
2019-05-09 01:14:53 +0300

    Added long description for package

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
+from os import path
+
+dirname = path.abspath(path.dirname(__file__))
+with open(path.join(dirname, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='osm2geojson',
     version='0.1',
     license='MIT',
     description='Parse OSM and Overpass JSON',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords = 'geometry gis osm parsing',
     author='Parfeniuk Mykola',
     author_email='mikola.parfenyuck@gmail.com',
