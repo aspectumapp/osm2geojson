e4711d74dc693b6a9f5c3fdc95833a5a1e18c0f3
rapkin <mikola.parfenyuck@gmail.com>
2020-05-11 13:27:25 +0300

    Use requirements.txt for deps

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from os import path
-
 from setuptools import setup
+try:
+    # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:
+    # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 dirname = path.abspath(path.dirname(__file__))
 with open(path.join(dirname, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
 
 setup(
     name='osm2geojson',
@@ -18,5 +27,5 @@ setup(
     author_email='mikola.parfenyuck@gmail.com',
     packages=['osm2geojson'],
     include_package_data=True,
-    install_requires=['shapely', 'requests']
+    install_requires=load_requirements("requirements.txt")
 )
