bd8660b6d92c4a849f6c17348b1ab1396db3840f
Mikola  Parfenyuck <mikola.parfenyuck@gmail.com>
2021-11-23 21:35:45 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def parse_requirements(filename):
 
 setup(
     name='osm2geojson',
-    version='0.1.29',
+    version='0.1.30',
     license='MIT',
     description='Parse OSM and Overpass JSON',
     long_description=long_description,
