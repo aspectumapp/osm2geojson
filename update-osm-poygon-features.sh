8eb0c9268848025fd62d955f029b0d2ed83a0822
Mikola  Parfenyuck <mikola.parfenyuck@gmail.com>
2024-03-25 13:57:22 +0200

    Handle issues with polygon tags mentioned in #7

--- update-osm-poygon-features.sh
+++ update-osm-poygon-features.sh
@@ -2,3 +2,12 @@ git submodule init
 git submodule update
 cp osm-polygon-features/polygon-features.json osm2geojson
 cp id-area-keys/areaKeys.json osm2geojson
+
+# TODO: Drop these patch lines when osm-polygon-features sub-module is updated
+# Remove the "wall" value from the "barrier" key using jq (https://github.com/aspectumapp/osm2geojson/issues/7#issuecomment-1967830306)
+jq --indent 4 '(.[] | select(.key == "barrier" and .polygon == "whitelist") | .values) -= ["wall"]' osm2geojson/polygon-features.json > tmp1.json
+# Add `highway=steps` to the blacklist (#7)
+jq --indent 4 '(.[] | select(.key == "highway" and .polygon == "blacklist") | .values) += ["steps"] | if map(.key == "highway" and .polygon == "blacklist") | any | not then . += [{"key": "highway", "polygon": "blacklist", "values": ["steps"]}] else . end' tmp1.json > tmp2.json
+
+mv tmp2.json osm2geojson/polygon-features.json
+rm tmp1.json
